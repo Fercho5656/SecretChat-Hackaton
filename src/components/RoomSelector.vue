399bc56d5aa57965c87130969c566da5b74a29c4
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-03 02:53:26 -0500

    Form to join a chat

---
+++ src/components/RoomSelector.vue
@@ -0,0 +1,36 @@
+<template>
+    <form>
+        <label>Introduce a Chat-ID</label>
+        <Input v-model="roomId" placeholder="Chat-ID" />
+        <Button>Join!</Button>
+    </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Input from './Input.vue';
+import Button from './Button.vue';
+
+const roomId = ref<string>('');
+</script>
+
+<style scoped>
+label {
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.25rem;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+form {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    max-width: 320px;
+}
+
+button {
+    width: 100%;
+}
+</style>
