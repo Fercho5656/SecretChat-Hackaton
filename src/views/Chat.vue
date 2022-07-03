7bc16483cbbbf39247dd3b731867f78bc79fe3b4
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-03 02:53:42 -0500

    Chat view

---
+++ src/views/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+    <NoUserHeader v-if="!userAvailable" />
+    <UserHeader v-else />
+    <RoomSelector v-if="userAvailable" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import NoUserHeader from '../components/NoUserHeader.vue'
+import RoomSelector from '../components/RoomSelector.vue';
+import UserHeader from '../components/UserHeader.vue';
+import { useUserStore } from '../store/user.store';
+
+const user = useUserStore();
+const userAvailable = ref();
+
+user.$subscribe(() => userAvailable.value = user.$state.user)
+</script>
+
+<style scoped>
+</style>
