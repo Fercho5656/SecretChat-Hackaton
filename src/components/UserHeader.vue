872f24f5894fb596f7c5ef7d3637163096f89d8f
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-03 02:53:05 -0500

    Header when there's a user

---
+++ src/components/UserHeader.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <strong>Hello, {{ user.user?.username }}!</strong>
+        <img :src="user.user?.avatar" :alt="user.user?.name">
+        <Button @click="logout">
+            <LogoutIcon />
+            Logout
+        </Button>
+    </header>
+</template>
+
+<script setup lang="ts">
+import LogoutIcon from './Icons/LogoutIcon.vue'
+import { useUserStore } from '../store/user.store'
+import Button from './Button.vue';
+const user = useUserStore()
+
+const logout = () => {
+    user.$state = {
+        user: null,
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    margin-bottom: 1rem;
+    width: 100vw;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 1rem;
+    background-color: #F3F4F6;
+    border-bottom: 1px solid #ddd;
+}
+
+strong {
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: var(--black-text);
+}
+
+img {
+    width: 3.5rem;
+    height: 3.5rem;
+    border-radius: 50%;
+    margin-right: 0.5rem;
+}
+
+button {
+    line-height: 1;
+    padding: 0.5rem;
+    display: inline-flex;
+    align-items: center;
+    gap: 0.25rem;
+}
+
+button:hover {
+    text-decoration: underline;
+}
+</style>
