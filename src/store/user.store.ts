56e22cc39dbcca89d7f6195c039f7b207baf2731
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-05 14:55:05 -0500

    Added interface to manage state as null or as User

--- src/store/user.store.ts
+++ src/store/user.store.ts
@@ -1,10 +1,14 @@
 import { defineStore } from 'pinia'
 import User from '../interfaces/User'
 
+interface IUserStore {
+  user: User | null
+}
+
 export const useUserStore = defineStore('user', {
-  state: () => {
+  state: (): IUserStore => {
     return {
-      user: {} as User | null
+      user: null
     }
   }
 })
