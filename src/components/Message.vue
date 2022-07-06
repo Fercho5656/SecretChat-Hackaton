7cb9c0269c373c831ca3232b65fbdbd68aa3ebf7
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-06 15:01:34 -0500

    Component to show a single message

---
+++ src/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{message.body}}</p>
+  <small>{{message.author}}</small>
+</template>
+
+<script setup lang="ts">
+import IMessage from '../interfaces/IMessage';
+interface Props {
+  message: IMessage
+}
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+</style>
