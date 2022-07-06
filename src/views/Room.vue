8202b4fd2d4028181d6e2192dca41f1b39bd5769
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-05 20:29:59 -0500

    Chat room view

---
+++ src/views/Room.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{ roomStore.room.uniqueName }}</h2>
+  <Conversation />
+  <ConversationInput />
+</template>
+
+<script setup lang="ts">
+import Conversation from '../components/Conversation.vue'
+import ConversationInput from '../components/ConversationInput.vue'
+import { onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
+import { useRoomStore } from '../store/room.store'
+
+const router = useRouter()
+const roomStore = useRoomStore()
+
+onBeforeMount(() => {
+  // if there's no room, redirect to the home page
+  if (roomStore.room == null) {
+    router.push('/')
+  }
+})
+</script>
+
+<style scoped>
+</style>
