5e3c059b84905169ca1db32792a2f10ff7ff9aaf
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-08 14:59:53 -0500

    Replaced anonymous token for supabase access token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,11 @@
+import supabase from "./supabase"
+
 export const getAccessToken = async (token: string): Promise<string> => {
+  const user = await supabase.auth.api.getUser(token)
+  const identity = user.user?.user_metadata.name
   const res = await fetch('http://localhost:3001/api/get-access-token', {
     headers: {
-      authorization: token
+      authorization: identity
     }
   })
 
