759af07c537519b304e4c77f84f1ee37f683ee9b
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-09 01:33:48 -0500

    Added prop to define button as primary or secondary

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,15 @@
 <template>
-    <button>
+    <button :class="type">
         <slot></slot>
     </button>
 </template>
 
 <script setup lang="ts">
+interface Props {
+    type?: 'primary' | 'secondary';
+}
+
+defineProps<Props>()
 </script>
 
 <style scoped>
@@ -15,18 +20,25 @@ button {
     color: var(--white-text);
     border: none;
     border-radius: 8px;
-    padding: 0.5rem 1rem;
+    padding: 0 1rem;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
     background-position: center;
     transition: background 0.8s;
 }
 
+button.secondary {
+    background-color: #fff;
+    color: var(--primary-color);
+}
+
 button:hover {
     background: #7458ed radial-gradient(circle, transparent 1%, #7458ed 1%) center/15000%;
+    color: var(--white-text);
 }
 
 button:active {
     background-color: #9b86f4;
+    color: var(--white-text);
     background-size: 100%;
     transition: background 0s;
 }
