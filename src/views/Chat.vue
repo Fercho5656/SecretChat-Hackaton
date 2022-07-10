eca4340c9628b14ea4d1f05c758a8b0f15b02698
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-10 11:11:00 -0500

    Replaced RoomSelector for MyChats

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,16 +1,16 @@
 <template>
-    <RoomSelector v-if="userAvailable" />
+    <MyChats />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import RoomSelector from '../components/RoomSelector.vue';
+import MyChats from '../components/MyChats.vue';
 import { useUserStore } from '../store/user.store';
 
 const user = useUserStore();
 const userAvailable = ref();
 
-user.$subscribe(() => userAvailable.value = user.$state.user)
+user.$subscribe(() => userAvailable.value = user.$state.user != null);
 </script>
 
 <style scoped>
