766433cdc0a5e78f2326384197cc63dd13c3c5e8
Fercho5656 <morenomoranfernando@gmail.com>
2022-07-17 18:47:11 -0500

    Header component for conversations

---
+++ src/components/ConversationHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <GoogleButton @click="onReturnChats">
+      <Arrow />
+    </GoogleButton>
+    <h2 v-if="conversationName">{{ conversationName }}</h2>
+    <button>⬅</button>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import GoogleButton from '../components/GoogleButton.vue'
+import { useRoomStore } from '../store/room.store';
+import Arrow from './Icons/Arrow.vue'
+
+const roomStore = useRoomStore()
+const router = useRouter()
+
+interface Props {
+  conversationName?: string
+}
+
+defineProps<Props>()
+
+const onReturnChats = () => {
+  roomStore.room = null
+  router.push('/chat')
+}
+</script>
+
+<style scoped>
+header {
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+  background-color: var(--secondary-color);
+  width: 100vw;
+  height: 20vh;
+  z-index: 10;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+}
+
+button>svg {
+  transform: scaleX(-1);
+}
+</style>
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,12 +1,11 @@
 <template>
-  <header>
-    <h2>{{ roomStore.room.uniqueName }}</h2>
-  </header>
+  <ConversationHeader :conversation-name="roomStore.room.uniqueName" />
   <Conversation />
   <ConversationInput />
 </template>
 
 <script setup lang="ts">
+import ConversationHeader from '../components/ConversationHeader.vue';
 import Conversation from '../components/Conversation.vue'
 import ConversationInput from '../components/ConversationInput.vue'
 import { onBeforeMount } from 'vue';
@@ -25,12 +24,4 @@ onBeforeMount(() => {
 </script>
 
 <style scoped>
-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1rem;
-  background-color: var(--secondary-color);
-  width: 100%;
-}
 </style>
